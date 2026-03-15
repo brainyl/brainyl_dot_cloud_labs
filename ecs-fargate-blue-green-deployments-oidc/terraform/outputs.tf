a5961025a810e0a69953ad3a1a4aa0f6910a190f
Kelvin Igbineweka <kelvin.brainyl@gmail.com>
2026-03-15 18:23:55 -0400

    add output

--- ecs-fargate-blue-green-deployments-oidc/terraform/outputs.tf
+++ ecs-fargate-blue-green-deployments-oidc/terraform/outputs.tf
@@ -7,3 +7,10 @@ output "alb_target_group_arn" {
   description = "ALB target group ARN for app"
   value       = aws_lb_target_group.blue_app_tg.arn
 }
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.app.name
+}
+output "ecs_service_name" {
+  value = aws_ecs_service.app.name
+}
