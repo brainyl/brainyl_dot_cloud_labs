77cc986a1def2bc85daaa3f26affe856b8007647
Kelvin Igbineweka <kelvin.brainyl@gmail.com>
2026-03-22 16:26:58 -0400

    Update variables.tf

--- ecs-fargate-blue-green-deployments-oidc/terraform/variables.tf
+++ ecs-fargate-blue-green-deployments-oidc/terraform/variables.tf
@@ -41,7 +41,7 @@ variable "ecs_memory" {
 }
 
 variable "app_version" {
-  default = "green-v2" # green-v2
+  default = "blue-v1" # green-v2
 }
 variable "container_image" {
   description = "container image"
